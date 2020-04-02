d5b5d725950beeed495775ce8404bf8dc6abd6a5
Martin Marko <marcus@gratex.com>
2020-04-02 20:36:32 +0200

    switching to wg version

--- tools/gdal/lib/gdal.js
+++ tools/gdal/lib/gdal.js
@@ -1,7 +1,7 @@
 const { spawnP2 } = require('./spawnP.js');
 const split = require('split2');
 
-const DMR_FILE = "/Volumes/data/_WORK/gis-projects/data/zbgis/DMR3-5_10/dmr3_5_10.tif"; //TODO:
+const DMR_FILE = "/Volumes/data/_WORK/gis-projects/data/zbgis/DMR3-5_10/dmr3_5_10.wg84.tif"; //TODO:
 
 const eleWGS84 = (lonLat) => spawnP2("gdallocationinfo", [DMR_FILE, '-valonly', '-wgs84'], {}, lonLat.map(({ lon, lat }) => `${lon} ${lat}`));
 
