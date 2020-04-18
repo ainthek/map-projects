f5eb17d51b98309819d7c5bac0d24e25072c555d
Martin Marko <marcus@gratex.com>
2020-04-18 14:18:01 +0200

    back to origina 3.5 dmr

--- tools/gdal/lib/gdal.js
+++ tools/gdal/lib/gdal.js
@@ -1,8 +1,8 @@
 const { spawnP2 } = require('./spawnP.js');
 const split = require('split2');
-
+const DMR_FILE = `${__dirname}/../../../../gis-projects/data/zbgis/DMR3-5_10/dmr3_5_10.tif`;
 //const DMR_FILE = `${__dirname}/../../../../gis-projects/data/zbgis/DMR3-5_10/dmr3_5_10.wg84.tif`; //TODO:
-const DMR_FILE = `${__dirname}/../../../../gis-projects/data/zbgis/esri-converted-dmr35-merged.tif`; 
+//const DMR_FILE = `${__dirname}/../../../../gis-projects/data/zbgis/esri-converted-dmr35-merged.tif`; 
 const eleWGS84 = (lonLat) => spawnP2("gdallocationinfo", [DMR_FILE, '-valonly', '-wgs84'], {}, lonLat.map(({ lon, lat }) => `${lon} ${lat}`));
 
 const git = module.exports = {
